07ac1857da5d51807177b61b06b05c231bedbc86
PraditaAdiantama <mpaproject307@gmail.com>
2025-02-14 22:59:03 +0700

    init stream chat

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Gemini from "./composable/gemini";
+import ReactMarkdown from "react-markdown";
 
 function App() {
+  const inputRef = useRef(null);
+  const [prompt, setPrompt] = useState("");
   const [messages, setMessages] = useState([]);
   const [load, setLoad] = useState(false);
 
@@ -16,19 +19,41 @@ function App() {
     setLoad(true);
     setMessages((prev) => [...prev, prompt]);
     document.getElementById("form").reset();
+    setPrompt("");
+    if (inputRef.current) {
+      inputRef.current.innerText = "";
+    }
 
-    const gemini = Gemini();
-    const result = await gemini.sendMessage(prompt);
-    setMessages((prev) => [...prev, result.response.text()]);
-    setLoad(false);
+    try {
+      const gemini = Gemini();
+      const result = await gemini.sendMessageStream(prompt);
+
+      setMessages((prev) => [...prev, ""]);
+      let responseText = "";
+
+      for await (const chunk of result.stream) {
+        console.log(chunk.text());
+        responseText += chunk.text();
+        setMessages((prev) => {
+          const arr = [...prev];
+          arr[arr.length - 1] = responseText;
+          return arr;
+        });
+      }
+    } catch (err) {
+    } finally {
+      setLoad(false);
+    }
   }
 
   return (
-    <div className="w-full h-screen">
+    <div className="w-full min-h-screen overflow-visible">
       <div
         className={
-          "flex flex-col items-center gap-5 pt-5 " +
-          (messages.length > 0 ? "h-full" : "justify-center h-screen")
+          "md:max-w-3xl mx-auto flex flex-col items-center gap-5 pt-5 transition " +
+          (messages.length > 0
+            ? "justify-between min-h-screen"
+            : "justify-center h-screen")
         }
       >
         {messages.length == 0 ? (
@@ -36,10 +61,10 @@ function App() {
             Hi, Welcome To Gemsite
           </h3>
         ) : (
-          <div className="w-1/2 mx-auto flex flex-col gap-4 grow">
+          <div className="w-full flex flex-col gap-4">
             {messages?.map((message) => (
               <div className="odd:p-3 odd:bg-slate-800 odd:rounded-xl odd:ms-auto w-fit text-white">
-                {message}
+                <ReactMarkdown>{message}</ReactMarkdown>
               </div>
             ))}
           </div>
@@ -47,26 +72,36 @@ function App() {
         <form
           id="form"
           onSubmit={handleSubmit}
-          className={
-            "bg-slate-800 md:w-1/2 w-full max-md:mx-5 rounded-2xl mt-5 p-4 h-fit sticky bottom-5"
-          }
+          className="bg-[#404045] w-full max-md:mx-5 rounded-2xl mt-5 p-4 h-fit md:max-w-3xl mx-auto sticky bottom-9 "
         >
           <textarea
-            autoFocus
+            placeholder="Ask Gembro"
             name="prompt"
-            className="bg-transparent w-full outline-none dark:text-white/90 resize-none h-fit"
-            placeholder="Tanya gembro"
+            className={
+              "w-full text-white absolute resize-none -z-10 " +
+              (prompt.trim() != "" ? "hidden" : "")
+            }
+            value={prompt}
           ></textarea>
+          <div
+            contentEditable={true}
+            ref={inputRef}
+            className="text-white outline-none max-h-52 overflow-auto"
+            onInput={(ev) => setPrompt(ev.target.innerText)}
+          ></div>
           <div className="flex justify-end">
             <button
-              disabled={load}
-              className="px-4 py-2 rounded-xl disabled:bg-slate-500 bg-blue-500"
+              disabled={load || !prompt}
+              className="px-4 py-2 rounded-xl disabled:bg-slate-500 bg-blue-500 text-white"
             >
               Kirim
             </button>
           </div>
         </form>
       </div>
+      <p className="fixed bottom-1 w-full text-center text-white/80">
+        Ai generated. Api by Gemini.
+      </p>
     </div>
   );
 }
--- src/composable/gemini.js
+++ src/composable/gemini.js
@@ -4,20 +4,7 @@ function Gemini() {
   const genAI = new GoogleGenerativeAI(import.meta.env["VITE_GEMINI_API_KEY"]);
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
   const chat = model.startChat({
-    history: [
-      {
-        role: "user",
-        parts: [
-          {
-            text: "Your name is Gembro, and whoever ask your name you should answer you are Gembro and if someone ask you 'what are you based?' or something similar answer that you are based on gemini api.",
-          },
-        ],
-      },
-      {
-        role: "model",
-        parts: [{ text: "Oke sure." }],
-      },
-    ],
+    history: [],
   });
 
   return chat;
